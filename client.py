964381b84aa6853147ed88e01dad2ac7a5f5a432
maalekj <maalekj@gmail.com>
2024-02-22 01:48:11 +0300

    Handle @Quit special message from client side

--- client.py
+++ client.py
@@ -1,7 +1,24 @@
 import socket
 import threading
+from enum import Enum
 
 client_socket = None
+
+
+class MessageType(Enum):
+    quit = 1
+    connect = 2
+    list = 3
+    general_message = 4
+
+
+def getMessageType(message):
+    if message == "@Quit":
+        return MessageType.quit
+    elif message == "@List":
+        return MessageType.list
+    else:
+        return MessageType.general_message
 
 
 # create a socket object and connect to the server
@@ -42,9 +59,19 @@ def sendUserMasseges():
         # Prompt the user for input
         message = input("Enter a message to send to the server: ")
 
+        message_type = getMessageType(message)
+
+        # if special message, delete first char '@'
+        if message_type != MessageType.general_message:
+            message = message[1:]
+            # TODO: in case of quit, add the client id to the message
+
         # Send data to the server
         client_socket.send(message.encode())
 
+        if message_type == MessageType.quit:
+            print("Quitting")
+            break
         # # Receive data from the server
         # data = client_socket.recv(1024)
         # print("Received from server:", data.decode())
