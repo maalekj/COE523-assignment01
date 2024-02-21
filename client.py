139c3e250ef8f420d0b9d72eba531768e8ca14ad
maalekj <maalekj@gmail.com>
2024-02-22 01:21:59 +0300

    Restructure client file to enable expanding later

--- client.py
+++ client.py
@@ -1,23 +1,49 @@
 import socket
+import threading
 
-# Create a socket object
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket = None
 
-# Define the server address and port
-server_address = ("localhost", 12345)
 
-# Connect to the server
-client_socket.connect(server_address)
+# create a socket object and connect to the server
+def connectToServer():
+    global client_socket
 
-# Prompt the user for input
-message = input("Enter a message to send to the server: ")
+    # Create a socket object
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Send data to the server
-client_socket.send(message.encode())
+    # Define the server address and port
+    server_address = ("localhost", 12345)
 
-# Receive data from the server
-data = client_socket.recv(1024)
-print("Received from server:", data.decode())
+    # Connect to the server
+    client_socket.connect(server_address)
+    print("Connected to server at", server_address)
 
-# Close the socket
-client_socket.close()
+
+# get user input and do action or send massege to the server
+def sendUserMasseges():
+    global client_socket
+
+    if client_socket is None:
+        print("Client socket is None, exiting")
+        return
+
+    while True:
+        # Prompt the user for input
+        message = input("Enter a message to send to the server: ")
+
+        # Send data to the server
+        client_socket.send(message.encode())
+
+        # # Receive data from the server
+        # data = client_socket.recv(1024)
+        # print("Received from server:", data.decode())
+
+    # # Close the socket
+    # client_socket.close()
+
+
+if __name__ == "__main__":
+    print("Welcome to the client!")
+    connectToServer()
+    # TODO: Create a thread to handle receiving messages from the server
+    sendUserMasseges()  # Run the main function
